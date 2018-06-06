265a4f8ccab02ffaf3486ceb75cfbe59fe200be9
Y4n <siyu@blocks.tech>
2018-06-06 16:39:49 +0800

    url about '/'

--- hello.py
+++ hello.py
@@ -4,9 +4,58 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    index
+    """
     return 'Index Page'
 
 
 @app.route('/hello')
 def hello():
+    """
+    hello world
+    """
     return 'Hello, World'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    """
+    user profile
+    """
+    # show the user profile for that user
+    return 'User %s' % username
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    """
+    post
+    """
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    """
+    subpath
+    """
+    # show the subpath after /path/
+    return 'Subpath %s' % subpath
+
+
+@app.route('/projects/')
+def projects():
+    """
+    projects
+    """
+    return 'The project page'
+
+
+@app.route('/about')
+def about():
+    """
+    about page
+    """
+    return 'The about page'
